e6c5206e8172918e01afe1fbe26249dbe941a7c8
Sonarie <sonarie1980@gmail.com>
2022-01-23 22:53:19 -0600

    start button works, minus answers showing

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,24 @@
-//select the start quiz button:
-// 1.timer begins
-// 2.displays first question
-var buttonEl = window.document.querySelector(".start");
-const timeH = document.querySelector("h3");
+var startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-Btn");
+const questionContainerEl = document.getElementById("question-container");
+var timeH = window.document.querySelector("h3");
+
+const questionEl = document.getElementById("question");
+const answerButtonsEl = document.querySelector("button.btn");
+
 let timeSecond = 75;
 timeH.innerHTML = timeSecond;
 
-console.dir(buttonEl);
-// console.dir(counter);
+let shuffledQuestions, currentQuestionIndex;
 
-buttonEl.addEventListener("click", function () {
+function startGame() {
+  startButton.classList.add("hide");
+  document.getElementById("info").style.display = "none";
+  questionContainerEl.classList.remove("hide");
+
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+
   const countDown = setInterval(() => {
     timeSecond--;
     timeH.innerHTML = timeSecond;
@@ -17,38 +26,77 @@ buttonEl.addEventListener("click", function () {
       clearInterval(countDown);
     }
   }, 1000);
-});
+  setNextQuestion();
+}
 
-//Commonly used data types DO Not inlude:
-//1. strings
-//2. booleans
-//3. alerts
-//4. numbers
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
-//The condition in an if/else statement is enclosed with____.
-//1. quotes
-//2. curly brackets
-//3. parenthesis
-//4. square brackets
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innterText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsEl.appendChild(button);
+  });
+}
 
-//Arrays in JavaScript can be used to store____.
-//1. numbers and strings
-//2. other arrays
-//3. booleans
-//4. all of the above
+function resetState() {
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  }
+}
 
-//A very useful tool used during development and debugging for printing content to the debugger is:
-//1. JaveScript
-//2. terminal/bash
-//3. for loops
-//4. console.log
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  Array.from(answerButtonsEl.children).forEach((button) => {});
+}
 
-//String values must be envlosed within _____ when being assigned to variables.
-//1. commas
-//2. curly brackets
-//3. quotes
-//4. parenthesis
+const questions = [
+  {
+    question: "Commonly used data types DO Not inlude:",
+    answers: [
+      { text: "Strings", correct: false },
+      { text: "Booleans", correct: false },
+      { text: "Alerts", correct: true },
+      { text: "Numbers", correct: false },
+    ],
+  },
+  //The condition in an if/else statement is enclosed with____.
+  //1. quotes
+  //2. curly brackets
+  //3. parenthesis
+  //4. square brackets
 
-//All Done!
-//Your final score is
-//Enter initials
+  //Arrays in JavaScript can be used to store____.
+  //1. numbers and strings
+  //2. other arrays
+  //3. booleans
+  //4. all of the above
+
+  //A very useful tool used during development and debugging for printing content to the debugger is:
+  //1. JaveScript
+  //2. terminal/bash
+  //3. for loops
+  //4. console.log
+
+  //String values must be envlosed within _____ when being assigned to variables.
+  //1. commas
+  //2. curly brackets
+  //3. quotes
+  //4. parenthesis
+
+  //All Done!
+  //Your final score is
+  //Enter initials
+];
+
+startButton.addEventListener("click", startGame);
